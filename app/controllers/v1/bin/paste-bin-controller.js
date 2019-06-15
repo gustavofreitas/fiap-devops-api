98ae55edf7fd011bf7cd8b70b22e3b4ae17992dd
Gustavo Freitas <gustavopozes.freitas@gmail.com>
2019-06-15 14:13:41 -0300

    fix

--- app/controllers/v1/bin/paste-bin-controller.js
+++ app/controllers/v1/bin/paste-bin-controller.js
@@ -13,7 +13,7 @@ function get(req, res, next) {
 
 function post(req,res, next){
   var id = req.params.pasteId;
-  this.pasteBinService_.delete(id).then( result => {
+  this.pasteBinService_.create(id).then( result => {
     if(result){
       this.pasteBinService_.getAll().then(list => {
         res.status(200).json(list)
@@ -25,7 +25,7 @@ function post(req,res, next){
 
 function remove(req, res, next) {
   var newPaste = req.body;
-  var list = this.pasteBinService_.create(newPaste).then( result => {
+  var list = this.pasteBinService_.delete(newPaste).then( result => {
     if(result){
       this.pasteBinService_.getAll().then(list => {
         res.status(200).json(list)
